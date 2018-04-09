3a6f1c988e8f426c10812ebb07ba876ed307bf70
Julie Dursema <jmdursema@gmail.com>
2018-04-09 11:30:01 -0600

    add current weekday to app state

--- src/App.js
+++ src/App.js
@@ -9,20 +9,21 @@ class App extends Component {
     this.state = {
       Month: '',
       Day: null,
-      Year: null
+      Year: null,
+      Weekday: ''
     }
   }
 
   componentWillMount(){
     let date = (new Date).toDateString()
     let dateArray = date.split(' ')
-    this.setState({Month: dateArray[1], Year: parseInt(dateArray[3]), Day: parseInt(dateArray[2])})
+    this.setState({Month: dateArray[1], Year: parseInt(dateArray[3]), Day: parseInt(dateArray[2]), Weekday: dateArray[0] })
   }
   render() {
     return (
       <div className="App">
       <h1>Calendar</h1>
-      <Month month = {this.state.Month}/>
+      <Month month = {this.state.Month} day = {this.state.Day} weekday= {this.state.Weekday}/>
       </div>
     );
   }
