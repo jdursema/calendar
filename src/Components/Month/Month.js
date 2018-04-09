b60b09fbca21231a7fa2ad16b4064a4ca16e32c6
Julie Dursema <jmdursema@gmail.com>
2018-04-09 10:03:47 -0600

    Add change month functionality

--- src/Components/Month/Month.js
+++ src/Components/Month/Month.js
@@ -6,22 +6,39 @@ class Month extends Component {
     super();
     this.state = {
       Months: ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"],
-      currentMonth: ''
+      Weekdays: ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"],
+      currentMonth: '',
+      currentMonthNumber: null
     }
   }
 
   componentWillMount() {
-    let fullMonth = this.state.Months.find((month) => {
+    let fullMonth = this.state.Months.find((month, index) => {
       return month.includes(this.props.month)
     })
     this.setState({currentMonth: fullMonth})
-
     }
+
+  changeMonth (event){ 
+    let monthIndex = this.state.Months.indexOf(this.state.currentMonth)
+    if(event.target.value === 'back'){
+      this.setState({currentMonth: this.state.Months[monthIndex-1] })
+    } else {
+      this.setState({currentMonth: this.state.Months[monthIndex+1] })
+    }
+  }
+
+  
 
   render () {
     return (
       <div>
+        <button onClick={(event) => this.changeMonth(event)} value='back'>Back</button>
         <h2>{this.state.currentMonth}</h2>
+        <button onClick={(event) => this.changeMonth(event)} value='next'>Next</button>
+        <table>
+
+        </table>
       </div>
     )
   }
